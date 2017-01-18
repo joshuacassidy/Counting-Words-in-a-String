180791cf6b124729bec2fc551ebb412fbd2ee2cc
joshuacassidy <Joshcassidy79@gmail.com>
2017-01-18 19:25:36 +0000

    second commit

--- CountingWordsInAString.rb
+++ CountingWordsInAString.rb
@@ -1,2 +1,4 @@
-string = gets.gsub!(/[^A-Za-z ]/, '').chomp().to_s.split(" ")
-  puts string.length > 0 ?  "The number of words in this string is #{string.length}" :  "there are no words in this string"
+string = gets
+words = string.dup.gsub!(/[^A-Za-z ]/, '').chomp().to_s.split(" ")
+vowels = string.dup.gsub!(/[^AEIOUaeiou]/, '').to_s
+puts string.length > 0 ?  "In this string there is #{words.length} words, #{words.length - 1} spaces, #{string.length - words.length} letters, #{vowels.length} vowels, #{(string.length - (words.length)) - vowels.length} constants in this string." :  "There are no words, letters or spaces in this string"
