5f2d79839128e24379e82b7c63824481f446dc87
joshuacassidy <Joshcassidy79@gmail.com>
2017-01-18 20:37:49 +0000

    fifth commit

--- CountingWordsInAString.rb
+++ CountingWordsInAString.rb
@@ -1,2 +1,4 @@
-string = gets
-puts string.length > 0 ?  "In this string there is #{string.dup.gsub!(/[^A-Za-z ]/, '').chomp().to_s.split(" ").length} words, #{string.dup.gsub!(/[^A-Za-z ]/, '').chomp().to_s.split(" ").length - 1} spaces, #{string.length - string.dup.gsub!(/[^A-Za-z ]/, '').chomp().to_s.split(" ").length} letters, #{string.dup.gsub!(/[^AEIOUaeiou]/, '').to_s.length} vowels, #{(string.length - (string.dup.gsub!(/[^A-Za-z ]/, '').chomp().to_s.split(" ").length)) - string.dup.gsub!(/[^AEIOUaeiou]/, '').to_s.length} constants in this string." : "There are no words, letters or spaces in this string"
+  string = gets
+    puts string.length > 0 ?  "In this string there is #{string.dup.gsub!(/[^A-Za-z ]/, '').chomp().to_s.split(" ").length} words, #{string.dup.gsub!(/[^A-Za-z ]/, '').chomp().to_s.split(" ").length - 1} spaces, #{string.length - string.dup.gsub!(/[^A-Za-z ]/, '').chomp().to_s.split(" ").length} letters, #{string.dup.gsub!(/[^AEIOUaeiou]/, '').to_s.length} vowels, #{(string.length - (string.dup.gsub!(/[^A-Za-z ]/, '').chomp().to_s.split(" ").length)) - string.dup.gsub!(/[^AEIOUaeiou]/, '').to_s.length} constants and #{string.gsub(/[^0-9]/, '').length} numbers in this string." : "There are no words, letters or spaces in this string"
+    x = "gets+-><*-+=%><&|"
+    puts x.gsub(/[^*-+=%><&|!^]/, '').length
