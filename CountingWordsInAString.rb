394cf9222c5f35021e17c5f4e02abb269e7abbed
joshuacassidy <Joshcassidy79@gmail.com>
2017-01-18 20:00:43 +0000

    third commit

--- CountingWordsInAString.rb
+++ CountingWordsInAString.rb
@@ -1,4 +1,4 @@
 string = gets
 words = string.dup.gsub!(/[^A-Za-z ]/, '').chomp().to_s.split(" ")
 vowels = string.dup.gsub!(/[^AEIOUaeiou]/, '').to_s
-puts string.length > 0 ?  "In this string there is #{words.length} words, #{words.length - 1} spaces, #{string.length - words.length} letters, #{vowels.length} vowels, #{(string.length - (words.length)) - vowels.length} constants in this string." :  "There are no words, letters or spaces in this string"
+puts string.length > 0 ?  "In this string there is #{string.dup.gsub!(/[^A-Za-z ]/, '').chomp().to_s.split(" ").length} words, #{string.dup.gsub!(/[^A-Za-z ]/, '').chomp().to_s.split(" ").length - 1} spaces, #{string.length - string.dup.gsub!(/[^A-Za-z ]/, '').chomp().to_s.split(" ").length} letters, #{vowels.length} vowels, #{(string.length - (words.length)) - vowels.length} constants in this string." :  "There are no words, letters or spaces in this string"
